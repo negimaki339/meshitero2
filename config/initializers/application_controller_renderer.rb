4b58a7a0732e47ab660764653915d23d28d76b80
negimaki339 <negimaki0620@gmail.com>
2022-02-09 14:03:24 +0000

    [画像投稿機能追加]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6ea142e56d30d7ea0faf325763f51a2720ad35257b5c736698efb02c98b286ef407520e44abdc6ffae9c540ff89bba91dbc5210019eba7b901f9aef75782e205'
